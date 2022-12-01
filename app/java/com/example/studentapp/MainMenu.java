7a62bd0ea17651a9b4749cbb6f41147b3fb8982d
alexts8 <98460373+alexts8@users.noreply.github.com>
2022-12-01 17:15:34 +0000

    Add files via upload

---
+++ app/java/com/example/studentapp/MainMenu.java
@@ -0,0 +1,16 @@
+package com.example.studentportal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainMenu extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainmenu);
+    }
+
+}
